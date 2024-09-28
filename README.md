73c9b749ff3ec2585baee2497906e9294a7dd755
felipesp.rocha@gmail.com <felipesp.rocha@gmail.com>
2024-09-28 15:46:22 -0300

    feat: add dino-funk and readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# music
-Music With Srudel REPL
+# Music
+Musical studies made with Strudel REPL. Just copy the code from music js files to [Strudel](https://strudel.cc/)
+or clone the [project](https://github.com/tidalcycles/strudel) and have fun ;) 
+
---
+++ dino-funk-remixed-wip.js
@@ -0,0 +1,33 @@
+samples(
+  {
+    bass:'https://cdn.freesound.org/previews/614/614637_2434927-hq.mp3',
+    disco:'https://cdn.freesound.org/previews/702/702142_15147295-lq.mp3',
+    
+  }
+)
+
+samples('bubo:waveforms');
+
+let drum = "bd*4, [- cp]*2, [- hh]*4";
+
+let machine = "RolandTR909";
+
+let pattern1 = stack (
+    sound(drum).bank(machine),
+    s('bass').loopAt(4)
+)
+
+let pattern2 = note("<[g3,b3,e4]!2 [a3,c3,e4] [b3,d3,f#4]>")
+    .n("<1 2 3 4 5 6 7 8 9 10>/2").room(0.5).size(0.9)
+    .s('wt_flute').velocity(0.25).often(n => n.ply(2))
+    .release(0.125).decay("<0.1 0.25 0.3 0.4>").sustain(0)
+    .cutoff(2000).cutoff("<1000 2000 4000>").fast(4)
+    ._scope();
+cat(
+  sound(drum).bank(machine),
+  pattern1,
+  sound(drum).bank(machine),
+  pattern2,
+  sound(drum).bank(machine),
+  pattern1,
+)
